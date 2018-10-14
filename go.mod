f464f05396e23d7300fcdcb9308883fb7de3810a
ErFUN KH <erfun.kh@gmail.com>
2018-10-14 14:10:48 +0330

    Part 2

--- go.mod
+++ go.mod
@@ -1,9 +1,13 @@
 module github.com/ErFUN-KH/go-grpc-http-rest-microservice
 
 require (
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-sql-driver/mysql v1.4.0
 	github.com/golang/protobuf v1.2.0
+	github.com/grpc-ecosystem/grpc-gateway v1.5.1
 	golang.org/x/net v0.0.0-20180826012351-8a410e7b638d
+	google.golang.org/genproto v0.0.0-20180817151627-c66870c02cf8
 	google.golang.org/grpc v1.15.0
 	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0 // indirect
+	gopkg.in/yaml.v2 v2.2.1 // indirect
 )
--- pkg/cmd/server.go
+++ pkg/cmd/server.go
@@ -10,14 +10,19 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/ErFUN-KH/go-grpc-http-rest-microservice/pkg/protocol/grpc"
+	"github.com/ErFUN-KH/go-grpc-http-rest-microservice/pkg/protocol/rest"
 	"github.com/ErFUN-KH/go-grpc-http-rest-microservice/pkg/service/v1"
 )
 
 // Config is configuration for Server
 type Config struct {
 	// gRPC server start parameters section
-	// gRPC is TCP port to listen by gRPC server
+	// GRPCPort is TCP port to listen by gRPC server
 	GRPCPort string
+
+	// HTTP/REST gateway start parameters section
+	// HTTPPort is TCP port to listen by HTTP/REST gateway
+	HTTPPort string
 
 	// DB Datastore parameters section
 	// DatastoreDBHost is host of database
@@ -37,6 +42,7 @@ func RunServer() error {
 	// get configuration
 	var cfg Config
 	flag.StringVar(&cfg.GRPCPort, "grpc-port", "", "gRPC port to bind")
+	flag.StringVar(&cfg.HTTPPort, "http-port", "", "HTTP port to bind")
 	flag.StringVar(&cfg.DatastoreDBHost, "db-host", "", "Database host")
 	flag.StringVar(&cfg.DatastoreDBUser, "db-user", "", "Database user")
 	flag.StringVar(&cfg.DatastoreDBPassword, "db-password", "", "Database password")
@@ -45,6 +51,10 @@ func RunServer() error {
 
 	if len(cfg.GRPCPort) == 0 {
 		return fmt.Errorf("invalid TCP port for gRPC server: '%s'", cfg.GRPCPort)
+	}
+
+	if len(cfg.HTTPPort) == 0 {
+		return fmt.Errorf("invalid TCP port for HTTP gateway: '%s'", cfg.HTTPPort)
 	}
 
 	// add MySQL driver specific parameter to parse date/time
@@ -64,6 +74,11 @@ func RunServer() error {
 	defer db.Close()
 
 	v1API := v1.NewToDoServiceServer(db)
+
+	// run HTTP gateway
+	go func() {
+		_ = rest.RunServer(ctx, cfg.GRPCPort, cfg.HTTPPort)
+	}()
 
 	return grpc.RunServer(ctx, v1API, cfg.GRPCPort)
 }
